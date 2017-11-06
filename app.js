c2a56889f12909f511eac80d52f14fa0b9e8ae8a
somil kumar <somil.kumar91@gmail.com>
2017-11-06 20:49:00 +0530

    showing lat and lon

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const KEY = `&key=${API_KEY}`;
 
 
 request({
-    url: `https://maps.google.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia${KEY}`,
+    url: `https://maps.google.com/maps/api/geocode/json?address=rodricks%20restorations%20bangalore${KEY}`,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address : ${body.results[0].formatted_address}`);
+    console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+    // console.log(JSON.stringify(body, undefined, 2));
 });
