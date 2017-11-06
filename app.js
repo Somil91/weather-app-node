c58fda17e78a7c2ca4dc8a2fceb0a89ef5c663cb
somil kumar <somil.kumar91@gmail.com>
2017-11-06 20:59:38 +0530

    Adding Arguments

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 
 const request = require('request');
+const yargs = require('yargs');
+
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv);
 
 const API_KEY = 'AIzaSyDJm4tctOf4GAHy5bEMu0rIrPpm4mbOM_s';
 const KEY = `&key=${API_KEY}`;
 
+const encodedAddress = encodeURIComponent(argv.address);
+
 
 request({
-    url: `https://maps.google.com/maps/api/geocode/json?address=rodricks%20restorations%20bangalore${KEY}`,
+    url: `https://maps.google.com/maps/api/geocode/json?address=${encodedAddress}${KEY}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address : ${body.results[0].formatted_address}`);
